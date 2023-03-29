2fae87c8587b387d0adca65fa8b6d3f36a1c8be1
hiruzent <akungame132109@gmail.comgit config --global user.email akungame132109@gmail.com>
2023-03-29 18:59:36 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alkatra:wght@700&family=Edu+NSW+ACT+Foundation:wght@700&family=Montserrat:wght@200&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="ramadhan_icon.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>RAMADHAN 2023!</title>
+</head>
+<body>
+    <!-- sidebar -->
+    <input type="checkbox" id="check">
+    <div class="sidebar">       
+            <h1>Menu</h1>
+        <ul>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#info">Info</a></li>
+            <li><a href="#calendar">Calendar</a></li>
+            <li><a href="">Gallery</a></li>
+        </ul>
+    </div>
+    <!-- bagian header -->
+    <header>
+        <div class="container">
+        <h1><a href="">Ramadhan</a></h1>
+        <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#info">Info</a></li>
+            <li><a href="#calendar">Calendar</a></li>
+            <li><a href="">Gallery</a></li>
+        </ul>
+        <!-- untuk hp -->
+        <label for="check" class="mobile-menu"><i class="fa-solid fa-bars fa-2x" style="color:  #19376D;"></i></label>
+        </div>
+    </header>
+    <!-- bagian calendar -->
+    <section class="calendar" id="calendar">
+        <div class="container">
+            <img src="imsyakiyah.jpg">
+            <div class="h2">
+            <h2>Jadwal Imsyakiyah</h2>
+            <h2>Puasa Ramadhan</h2>
+            <h2>1444H</h2>
+        </div>
+    </section>
+
+    <!-- bagian Info -->
+    <section class="info" id="info">
+        <div class="container">
+            <div class="h2">
+            <img src="bulanbintang.png">
+            <h1>Informasi</h1>
+            <h2>Bulan suci Ramadhan adalah bulan yang penuh keberkahan, ampunan dan rakhmat serta kasih sayang dari Allah SWT. Selain menjadikan doa lebih mustajab, salah satu keutamaan puasa Ramadhan adalah sebagai pelindung dari setan. Setan akan selalu berusaha untuk menggoda manusia, agar mereka melakukan hal-hal yang dilarang dalam agama Islam. Pada bulan ini, umat Muslim di seluruh dunia melakukan ibadah puasa (saum) dan memperingati wahyu pertama yang turun kepada Nabi Muhammad menurut keyakinan umat Muslim.</h2>
+        </div>
+    </section>
+
+    <!-- bagian Gallery -->
+    <section class="info" id="info">
+        <div class="container">
+            <h1>Informasi</h1>
+            <div class="h2">
+            <img src="bulanbintang.png">
+            <h2>Bulan suci Ramadhan adalah bulan yang penuh keberkahan, ampunan dan rakhmat serta kasih sayang dari Allah SWT.Selain menjadikan doa lebih mustajab, salah satu keutamaan puasa Ramadhan adalah sebagai pelindung dari setan. Setan akan selalu berusaha untuk menggoda manusia, agar mereka melakukan hal-hal yang dilarang dalam agama Islam.</h2>
+        </div>
+    </section>
+</body>
+<footer>
+    <h1>Copyrigt by Naufal Fadhilah F</h1>
+    <h2>#AntiBootstrap</h2>
+</footer>
+</html>
